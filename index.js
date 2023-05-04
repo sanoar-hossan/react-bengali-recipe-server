ff7cdb553d630820d0fd4c812d21f9e80a1d5844
chayan <admin@ditskill.com>
2023-05-04 10:12:12 +0600

    id updated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const app =express();
 const port = process.env.PORT||5000;
+const cors = require('cors')
 
 const chef=require('./data/chef.json')
 const recipe=require('./data/recipe.json')
 app.get('/',(req,res)=>{
 res.send('news running')
 });
+
+app.use(cors());
 
 app.get('/chefdetails',(req,res)=>{
     res.send(chef);
